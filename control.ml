85485a58d3e62469a8308d43a1a9a48fd6f2846d
Andreas Rossberg <rossberg@mpi-sws.org>
2024-12-11 11:40:20 +0100

    Minor reformatting

--- control.ml
+++ control.ml
@@ -18,25 +18,26 @@ struct
     | Move of Cave.direction option * bool
     | Command of char
 
-  let last_key = ref None
-  let last_pad = ref None
+  let prev_key_cmd = ref None
+  let prev_pad_cmd = ref None
 
   let dpad l h = if l then -1 else if h then +1 else 0
   let axis v = if v < -0.9 then -1 else if v > +0.9 then +1 else 0
 
   let poll () =
-    let last = !last_key in
     let key_opt, shift = Api.poll_key control in
-    last_key := key_opt;
+    let prev_cmd = !prev_key_cmd in
+    prev_key_cmd := key_opt;
     match key_opt with
     | Some key ->
+      let open Api in
       (match key with
-      | Api.(Arrow Up) | Char ('W' | 'Z' | '8') -> Some (Move (Some Up, shift))
-      | Api.(Arrow Left) | Char ('A' | 'Q' | '4') -> Some (Move (Some Left, shift))
-      | Api.(Arrow Down) | Char ('S' | '2') -> Some (Move (Some Down, shift))
-      | Api.(Arrow Right) | Char ('D' | '6') -> Some (Move (Some Right, shift))
-      | Api.Char ('X' | '5') -> Some (Move (None, shift))
-      | Api.Char c when Api.is_buffered_key || last <> Some key ->
+      | Arrow Up | Char ('W' | 'Z' | '8') -> Some (Move (Some Up, shift))
+      | Arrow Left | Char ('A' | 'Q' | '4') -> Some (Move (Some Left, shift))
+      | Arrow Down | Char ('S' | '2') -> Some (Move (Some Down, shift))
+      | Arrow Right | Char ('D' | '6') -> Some (Move (Some Right, shift))
+      | Char ('X' | '5') -> Some (Move (None, shift))
+      | Char c when Api.is_buffered_key || prev_cmd <> Some key ->
         Some (Command c)
       | _ -> None
       )
@@ -49,10 +50,13 @@ struct
       | +1, _ -> Some (Move (Some Right, a))
       | _, -1 -> Some (Move (Some Up, a))
       | _, +1 -> Some (Move (Some Down, a))
-      | _, _ when b && !last_pad <> Some ' ' -> last_pad := Some ' '; Some (Move (None, a))
-      | _, _ when x && !last_pad <> Some 'P' -> last_pad := Some 'P'; Some (Command 'P')
-      | _, _ when y && !last_pad <> Some 'K' -> last_pad := Some 'K'; Some (Command 'K')
+      | _, _ when b && !prev_pad_cmd <> Some ' ' ->
+        prev_pad_cmd := Some ' '; Some (Move (None, a))
+      | _, _ when x && !prev_pad_cmd <> Some 'P' ->
+        prev_pad_cmd := Some 'P'; Some (Command 'P')
+      | _, _ when y && !prev_pad_cmd <> Some 'K' ->
+        prev_pad_cmd := Some 'K'; Some (Command 'K')
       | _, _ when b || x || y -> None
-      | _, _ -> last_pad := None; None
+      | _, _ -> prev_pad_cmd := None; None
 
 end
